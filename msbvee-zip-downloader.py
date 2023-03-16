89342ecbf038d6c789a27cdbabb8912f87b54e59
OmkarBhosekar7711 <67870002+OmkarBhosekar7711@users.noreply.github.com>
2023-03-16 19:59:58 +0530

    Update msbvee-zip-downloader.py

--- msbvee-zip-downloader.py
+++ msbvee-zip-downloader.py
@@ -1,51 +1,58 @@
+import requests
 from requests_html import HTMLSession
 import wget
-import requests
 import pathlib
-from urllib.error import ContentTooShortError
 
-session = HTMLSession()
-
-
-res = session.get('http://www.msbve.gov.in/msbve/html/samplepaper.htm').html.absolute_links # Set of urls
-path = pathlib.Path.cwd()
-    
-def format_file(filename):
-    whitespace_to_underscore = filename.replace(" ", "_")
-    file_name = whitespace_to_underscore.lower() # lower case 
-    return file_name
-
-def does_not_exists(filename):
-    file = path / filename
-    if not file.exists():
-        return True
+def get_absolute_links(url):
+    try:
+        session = HTMLSession()
+        links = session.get(url).html.absolute_links
+        return links
+    except Exception as e:
+        print(e)
     else:
-        return False
+        links = []
 
-def get_file_size(url):
+def download(url, filename):
+    print(f"Downloading {filename}")
+    try:
+        wget.download(url, out=filename)
+    except Exception as e:
+        print(e)
+
+def file_size_in_mb(url):
     response = requests.head(url)
-    size = round(
-        int(response.headers['content-length'].strip()) / (1024*1024)
-    ) # file size in mb default in bytes
+    content_length = response.headers['content-length'].strip()
+    size = round(int(content_length) / (1024*1024)) # bytes -> Mb 
     return size
 
-    
-for url in res:
-    if url.endswith('.zip'):
-        file_name = wget.detect_filename(url)
-        file = format_file(file_name)
-        file_not_exists = does_not_exists(file)
-        size = get_file_size(url)
-        if file_not_exists:
-            ask = input(f"Size of [{file}] is [~={size} mb] do you wish to continue? (y/n) default(y): ") or 'y'
-            print(ask)
-            if ask == 'y':
-                try:
-                    print(f"\nDownloading {file}[~= {size} mb]")
-                    wget.download(url, out=file)
-                except ContentTooShortError as error:
-                    print("Content Short Error trying again")
-                    wget.download(url, out=file)
+def get_file_name(url):
+    filename = wget.detect_filename(url=url).replace(" ", "_").lower() # file name
+    return filename
 
+
+if __name__ == "__main__": 
+    FILE_DOWNLOAD_SIZE = 30 # Mb
+    path = pathlib.Path.cwd()
+    links = get_absolute_links('http://www.msbve.gov.in/msbve/html/samplepaper.htm')
+    zip_urls = [link for link in links if link.endswith('.zip')]
+    
+    for url in zip_urls:
+        filename = get_file_name(url)
+        size = file_size_in_mb(url)
+        file = path / filename
+        
+        if file.exists():
+            print(f"file already exists {filename}")
         else:
-            print(f"File [{file}] already exists")
+            if size > FILE_DOWNLOAD_SIZE: # are you interested in downloading large files ?
+                ask = input(f"Do you wish to download {size}mb file? (y/n): ")
+                if ask == "y":
+                    download(url, filename=filename)
+                else:
+                    continue
+            else:
+                    download(url, filename=filename)
+
+    print() # \r + \n
+    
